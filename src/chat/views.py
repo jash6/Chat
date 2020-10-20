42184832051c1cb7fd9a70c3a1b4d13552af0a9a
jashmakhija <jashmakhija06@gmail.com>
2020-10-20 21:38:36 +0530

    Frontend  to be worked upon basic Backend done

--- src/chat/views.py
+++ src/chat/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
 
 # Create your views here.
-
+User = get_user_model()
 def index(request):
     return render(request, 'chat/index.html')
 
+@login_required
 def room(request, room_name):
     return render(request, 'chat/room.html', {
-        'room_name': room_name
+        'room_name': room_name,
+        'username': request.user.username
     })
